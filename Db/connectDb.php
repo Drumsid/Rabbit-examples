1ee244c57a01ec07e03a39a0bc948d0c6ea423c1
denis.soloduhin <denis.soloduhin@simbirsoft.com>
2022-03-08 14:02:22 +0300

    refactoring Db

--- Db/connectDb.php
+++ Db/connectDb.php
@@ -9,14 +9,6 @@ try {
     // make a database connection
     $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-//    if ($pdo) {
-//        echo "Connected to the $db database successfully!";
-//    }
 } catch (PDOException $e) {
     die($e->getMessage());
 }
-//finally {
-//    if ($pdo) {
-//        $pdo = null;
-//    }
-//}
--- Db/testDb.php
+++ Db/testDb.php
@@ -19,24 +19,3 @@ if ($pdo) {
 } else {
     echo "ERROR!";
 }
-//$host= 'postgres';
-//$db = 'rabbit_db';
-//$user = 'rabbit_user';
-//$password = 'secret';
-//
-//try {
-//    $dsn = "pgsql:host=$host;port=5432;dbname=$db;";
-//    // make a database connection
-//    $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-//
-//    if ($pdo) {
-//        echo "Connected to the $db database successfully!";
-//    }
-//} catch (PDOException $e) {
-//    die($e->getMessage());
-//}
-//finally {
-//    if ($pdo) {
-//        $pdo = null;
-//    }
-//}
